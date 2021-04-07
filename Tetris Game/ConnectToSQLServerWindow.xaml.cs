8ee538599ee0b2206077d124b6ef8a1304899a38
Vitor Ochoa <vitorochoa12@gmail.com>
2021-04-07 10:22:27 -0300

    Tetris Game

---
+++ Tetris Game/ConnectToSQLServerWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Tetris
+{
+    /// <summary>
+    /// Interaction logic for ConnectToSQLServerWindow.xaml
+    /// </summary>
+    public partial class ConnectToSQLServerWindow : Window
+    {
+
+        SCORETETRIS s = new SCORETETRIS();
+
+        public ConnectToSQLServerWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.ListarScores();
+        }
+
+        private void ListarScores()
+        {
+            using(dbSEQUORBS0023Entities3 dados = new dbSEQUORBS0023Entities3())
+            {
+                var consulta = dados.SCORETETRIS;
+                myDataGrid.ItemsSource = consulta.OrderByDescending(s => s.Score).Take(10).ToList();
+            }
+        }
+
+        
+
+        private void btnReturnInicio_Click(object sender, RoutedEventArgs e)
+        {
+            Window inicio = new Inicio();
+            this.Close();
+            inicio.Show();
+        }
+    }
+}
---
+++ Tetris Game/Inicio.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Threading;
+
+namespace Tetris
+{
+    /// <summary>
+    /// Interaction logic for Inicio.xaml
+    /// </summary>
+    public partial class Inicio : Window
+    {
+
+        
+        public Inicio()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Window w = new Tetris.MainWindow();
+            this.Close();
+            w.Show();
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Window s = new ConnectToSQLServerWindow();
+            this.Close();
+            s.Show();
+            
+        }
+
+
+
+    }
+}
